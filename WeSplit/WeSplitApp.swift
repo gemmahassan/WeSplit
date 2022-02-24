83d2b20294d963cb0b4bbe9c1898547f0aebd8b3
Hassan, Gemma <Gemma.Hassan@arity.com>
2022-02-24 20:17:41 +0000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by gfaul on 24/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
